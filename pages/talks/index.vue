aa7cc4d1ffba92030c21e4d35c397b77496c6e29
ct-martin <ct-martin@users.noreply.github.com>
2018-11-22 20:34:19 -0500

    Import talks

---
+++ pages/talks/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <article class="container">
+    <h1>Talks &amp; Workshops</h1>
+    <h2>RITlug</h2>
+    <b-list-group>
+      <b-list-group-item to="/talks/ritlug-overview-distributed-computing" class="flex-column align-items-start">
+        <h3 class="mb-1">Overview of Distributed Computing <small>(Talk)</small></h3>
+        <p class="mb-1">An overview of distributed computing, such as virtualization and containers, how we use them, and why.</p>
+      </b-list-group-item>
+        <b-list-group-item to="/talks/ritlug-self-hosted" class="flex-column align-items-start">
+          <h3 class="mb-1">Building Your Own Self-Hosted Infrastructure (Using Linux) <small>(Workshop)</small></h3>
+          <p class="mb-1">In this workshop, we will be creating a personal server that you can easily deploy yourself at home on a Raspberry Pi or a VPS (the cloud).</p>
+        </b-list-group-item>
+    </b-list-group>
+  </article>
+</template>
---
+++ pages/talks/ritlug-overview-distributed-computing.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="container">
+    <h1>Overview of Distributed Computing <small>(Talk, RITlug)</small></h1>
+
+    <p>An overview of distributed computing, such as virtualization and containers, how we use them, and why.</p>
+    <PDFViewer src="https://ritlug.com/talks/slides/2017-w10-fall-overview-of-distributed-computing.pdf" />
+    <br />
+    <a href="https://ritlug.com/talks/2017/11/03/overview-distributed-computing.html" target="_blank" class="btn btn-primary w-100 d-block">RITlug's Talk Page</a>
+    <a href="https://ritlug.com/talks/slides/2017-w10-fall-overview-of-distributed-computing.pdf" target="_blank" class="btn btn-primary w-100 d-block" style="margin-top:.25rem;">Direct link to slides</a>
+
+    <div class="card" style="margin-top:1rem;">
+      <div class="card-body">
+        <h4 class="card-title">License</h4>
+        <p>This talk is available under <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">CC BY-SA 4.0</a></p>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+import PDFViewer from '~/components/PDFViewer.vue'
+
+export default {
+  components: {
+    PDFViewer
+  }
+}
+</script>

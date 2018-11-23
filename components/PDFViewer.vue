838018bf6b023d9f1cd7065dee3be43104f269a7
ct-martin <ct-martin@users.noreply.github.com>
2018-11-22 20:34:06 -0500

    PDF.js Component

---
+++ components/PDFViewer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" :src="srcFull"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['src'],
+  computed: {
+    srcFull() {
+      // Google Drive Viewer: `http://docs.google.com/gview?url=${this.src}&embedded=true`
+      // PDF.js: `https://mozilla.github.io/pdf.js/web/viewer.html?file=${this.src}`
+      return `http://docs.google.com/gview?url=${this.src}&embedded=true`;
+    }
+  }
+}
+</script>

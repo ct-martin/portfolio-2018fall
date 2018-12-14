b26d18ad6bbcd66e6a4688d370c482737b8fa144
ct-martin <ct-martin@users.noreply.github.com>
2018-12-14 14:15:28 -0500

    Add about page

---
+++ pages/about.vue
@@ -0,0 +1,126 @@
+<template>
+  <article class="container">
+    <h1 id="about">About</h1>
+
+    <p>Student studying New Media Interactive Development at the Rochester Institute of Technology.</p>
+
+    <h2 id="contact">Contact Info</h2>
+    <ul class="list-group">
+      <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-github"></span> GitHub: <a href="https://github.com/ct-martin" target="_blank">@ct-martin</a></li>
+      <!--sse--><li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-envelope"></span> Email: <a href="mailto:hello@ctmartin.me">hello@ctmartin.me</a></li>
+      <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-comments-o"></span> IRC: ctmartin in #ritlug, #rit-foss on <a href="https://freenode.net/" target="_blank">FreeNode</a>.</li>
+      <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-handshake-o"></span> Employers: I can be found on RIT's Handshake and LinkedIn. Email me for a link and/or resume.</li><!--/sse-->
+    </ul>
+    <br />
+    <h2 id="nmid">What is New Media Interactive Development?</h2>
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-4">
+        <div class="card" style="margin-top: .75rem;">
+          <div class="card-body">
+            <blockquote class="blockquote">
+            <p class="mb-0">All types of emerging content &amp; experiences made possible by computing &amp; connectivity.</p>
+            <!-- <footer class="blockquote-footer">Slides from a course</footer> -->
+            </blockquote>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 col-lg-4">
+        <div class="card" style="margin-top: .75rem;">
+          <div class="card-body">
+            <blockquote class="blockquote">
+            <p class="mb-0">New Media Interactive Development uses a toolbox that includes code, connectivity, data, aesthetics, &amp; other new &amp; evolving approaches.</p>
+            <!-- <footer class="blockquote-footer">Slides from a course</footer> -->
+            </blockquote>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 col-lg-4">
+        <div class="card" style="margin-top: .75rem;">
+          <div class="card-body">
+            <blockquote class="blockquote">
+            <p class="mb-0">The field of new media explores new and evolving technologies... to create useful, delightful, and engaging experiences.</p>
+            <footer class="blockquote-footer"><a href="https://www.rit.edu/gccis/igm/bs-new-media-interactive-development-overview">IGM Degree Overview</a></footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br />
+    <h2 id="skills">Skills</h2>
+    <div class="row">
+      <div class="col-12 col-md-4 col-lg-3">
+        <div class="card" style="margin-top: .75rem;">
+          <div class="card-body">
+            <h4 class="card-title">Languages</h4>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-terminal"></span> Bash</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-code"></span> C#</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-code"></span> C++</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-code"></span> Java</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> <strong>JavaScript</strong> (inc. Node.JS)</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> <strong>PHP</strong></li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-terminal"></span> PowerShell</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-code"></span> <strong>Processing</strong> (Java, p5.js)</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-code"></span> Python</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-12 col-md-4 col-lg-3">
+        <div class="card" style="margin-top: .75rem;">
+          <div class="card-body">
+            <h4 class="card-title">Technologies</h4>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-terminal"></span> Ansible</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> Apache</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-tasks"></span> Continuous Integration (CI; Jenkins)</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-code-branch"></span> Git</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-database"></span> MariaDB (MySQL)</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-sync"></span> MVC</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> Networking</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-hdd"></span> RAID(Z)</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> Snort (NIDS)</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> Virtualization (QEMU/KVM-based, ESXi)</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-12 col-md-4 col-lg-3">
+        <div class="card" style="margin-top: .75rem;">
+          <div class="card-body">
+            <h4 class="card-title">Platforms</h4>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> <strong>Docker</strong></li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> DD-WRT</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> <strong>FreeNAS</strong></li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-linux"></span> <strong>Linux</strong> (Debian, Fedora, Ubuntu)</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-apple"></span> macOS</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-windows"></span> Microsoft Windows</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> <strong>pfSense</strong></li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span> VMware ESXi</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-12 col-md-4 col-lg-3">
+        <div class="card" style="margin-top: .75rem;">
+          <div class="card-body">
+            <h4 class="card-title">Design</h4>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-file-image-o"></span> Adobe Illustrator</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-file-image-o"></span> Adobe Photoshop</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-file-audio-o"></span> Cakewalk SONAR</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-css3"></span> CSS3</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-html5"></span> HTML5</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-joomla"></span> Joomla!</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-file-alt"></span> Microsoft Office</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-gamepad"></span> Unity</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-gamepad"></span> Unreal Engine</li>
+            <li class="list-group-item"><span aria-hidden="true" class="fa fa-fw fa-wordpress"></span> WordPress</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>

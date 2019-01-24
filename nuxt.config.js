66ab62e4ad8418b554c801f7cf88a91d8ef4f3d1
ct-martin <ct-martin@users.noreply.github.com>
2019-01-23 23:01:50 -0500

    Change page title in Nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: "Portfolio | Christian Martin",
     lang: 'en',
     meta: [
       { charset: 'utf-8' },

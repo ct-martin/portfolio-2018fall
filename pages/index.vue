7c3a61fb43663ca9e215871b8bc23acb155c746b
ct-martin <ct-martin@users.noreply.github.com>
2019-02-16 00:07:16 -0500

    Hero Cards (WIP)

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,24 @@
   <article class="container">
     <p>Student, Developer, Creative Technologist, Computing Generalist; Studying New Media Interactive Development at RIT.</p>
   </article>
+
+  <div class="container-fluid" id="heroCards" style="background:forestgreen;padding-top:2em;padding-bottom:2em;margin-top:2em;margin-bottom:1em;">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-4" v-for="project in this.$store.state['content'].list.filter(item => item.tags.includes('home-hero'))" :key="project.name">
+          <nuxt-link class="card" :to="project.url">
+            <div class="card-body">
+              <h4>{{project.name}}</h4>
+              <p class="card-text">{{project.description}}</p>
+              <nuxt-link :to="project.url" class="btn btn-primary d-block" style="color:rgba(255,255,255,0.9)">More</nuxt-link>
+            </div>
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br />
+
   <div class="container" id="homeCards">
     <div class="row">
       <div class="col-12 col-md-6">

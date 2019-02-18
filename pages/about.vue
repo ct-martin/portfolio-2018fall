6a07860d16797237ef607f4c5599223c863765dd
ct-martin <ct-martin@users.noreply.github.com>
2019-02-18 16:14:46 -0500

    About page updates

--- pages/about.vue
+++ pages/about.vue
@@ -1,101 +1,139 @@
 <template>
   <article class="container">
-    <h1 class="title">About</h1>
+    <div class="print-hidden">
+      <h1 class="title">About</h1>
 
-    <p>Student, Developer, Creative Technologist, Computing Generalist; Studying New Media Interactive Development at RIT.</p>
+      <p>Full Stack Developer, Creative Technologist, Computing Generalist; Studying New Media Interactive Development at Rochester Institute of Technology.</p>
 
-    <h2 class="title" style="margin-bottom:.25em;margin-top:.5em;">Contact Info</h2>
-    <div class="panel">
-      <div class="panel-block">
-        <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-github"></span></span>
-        GitHub: <a href="https://github.com/ct-martin" target="_blank">@ct-martin</a>
+      <h2 class="title" style="margin-bottom:.25em;margin-top:.5em;">Contact Info</h2>
+      <div class="panel">
+        <div class="panel-block">
+          <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-github"></span></span>
+          <span><a href="https://github.com/ct-martin" target="_blank">GitHub: @ct-martin</a></span>
+        </div>
+        <!--sse-->
+          <div class="panel-block">
+            <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-envelope"></span></span>
+            <span><a href="mailto:hello@ctmartin.me">Email: hello@ctmartin.me</a></span>
+          </div>
+          <div class="panel-block">
+            <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-comments-o"></span></span>
+            <span>IRC: ctmartin on <a href="https://freenode.net/" target="_blank">FreeNode</a> &amp; <a href="https://matrix.org/" target="_blank">Matrix</a></span>
+          </div>
+          <div class="panel-block">
+            <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-linkedin-square"></span></span>
+            <span><a href="https://www.linkedin.com/in/ct-martin/" target="_blank">LinkedIn: /in/ct-martin</a></span>
+          </div>
+          <div class="panel-block">
+            <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-handshake-o"></span></span>
+            <span><a href="https://rit.joinhandshake.com/users/6938294" target="_blank">Handshake</a></span>
+          </div>
+        <!--/sse-->
       </div>
-      <!--sse-->
-        <div class="panel-block">
-          <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-envelope"></span></span>
-          Email: <a href="mailto:hello@ctmartin.me">hello@ctmartin.me</a>
+
+      <h2 class="title" style="margin-bottom:.25em;margin-top:.5em;">Skills</h2>
+      <div id="skillset" class="columns">
+        <div class="column">
+          <div class="card" style="margin-top: .75rem;">
+            <div class="card-content">
+              <h4 class="subtitle">Languages</h4>
+            </div>
+            <div class="panel">
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> C#</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-css3"></span></span> CSS3</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-html5"></span></span> HTML5</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> Java</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> <span><strong>JavaScript</strong> (inc. Node.JS)</span></div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <strong>PHP</strong></div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> <strong>Processing</strong></div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> Python</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> React</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> Vue</div>
+            </div>
+          </div>
         </div>
-        <div class="panel-block">
-          <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-comments-o"></span></span>
-          IRC: ctmartin in #ritlug, #rit-foss on <a href="https://freenode.net/" target="_blank">FreeNode</a>
+        <div class="column">
+          <div class="card" style="margin-top: .75rem;">
+            <div class="card-content">
+              <h4 class="subtitle">Technologies</h4>
+            </div>
+            <div class="panel">
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <strong>Apache</strong></div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-tasks"></span></span> Continuous Integration (CI)</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code-fork"></span></span> <span><strong>Git</strong>, inc. GitHub &amp; GitLab</span></div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> HAProxy</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-database"></span></span> MariaDB/<strong>MySQL</strong></div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-database"></span></span> MongoDB</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <strong>Nginx</strong></div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-database"></span></span> Redis</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> Software-Defined Networking</div>
+            </div>
+          </div>
         </div>
-        <div class="panel-block">
-          <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-handshake-o"></span></span>
-          Employers: I can be found on RIT's Handshake and LinkedIn. Email me for a link and/or resume
+        <div class="column">
+          <div class="card" style="margin-top: .75rem;">
+            <div class="card-content">
+              <h4 class="subtitle">Platforms</h4>
+            </div>
+            <div class="panel">
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> Cloudflare</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <strong>Docker</strong></div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> Heroku</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-linux"></span></span> <span><strong>Linux</strong>; CentOS, Debian, Fedora, Ubuntu</span></div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <span><strong>Virtualization</strong> (QEMU/KVM-based, ESXi)</span></div>
+            </div>
+          </div>
         </div>
-      <!--/sse-->
+        <div class="column">
+          <div class="card" style="margin-top: .75rem;">
+            <div class="card-content">
+              <h4 class="subtitle">Design</h4>
+            </div>
+            <div class="panel">
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-file-image-o"></span></span> Adobe Illustrator</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-file-image-o"></span></span> Adobe Photoshop</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-file-image-o"></span></span> Adobe XD</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> MVC</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <span><strong>Responsive</strong> &amp; <strong>Mobile-First</strong> Design</span></div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-gamepad"></span></span> Unity</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-gamepad"></span></span> Unreal Engine</div>
+              <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-wordpress"></span></span> WordPress</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <h2 class="title" style="margin-bottom:.25em;margin-top:.5em;">Skills</h2>
-    <div class="columns">
-      <div class="column">
-        <div class="card" style="margin-top: .75rem;">
-          <div class="card-content">
-            <h4 class="subtitle">Languages</h4>
-          </div>
-          <div class="panel">
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> C#</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-css3"></span></span> CSS3</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-html5"></span></span> HTML5</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> Java</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> <span><strong>JavaScript</strong> (inc. Node.JS)</span></div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <strong>PHP</strong></div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> <strong>Processing</strong></div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> Python</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> React</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code"></span></span> Vue</div>
-          </div>
+    <div class="print-only">
+      <h1 class="title">Christian Martin</h1>
+      <div class="panel">
+        <div class="panel-block">
+          <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-github"></span></span>
+          Portfolio: https://ctmartin.me
         </div>
-      </div>
-      <div class="column">
-        <div class="card" style="margin-top: .75rem;">
-          <div class="card-content">
-            <h4 class="subtitle">Technologies</h4>
-          </div>
-          <div class="panel">
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <strong>Apache</strong></div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-tasks"></span></span> Continuous Integration (CI)</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-code-fork"></span></span> <strong>Git</strong>, inc. GitHub &amp; GitLab</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> HAProxy</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-database"></span></span> MariaDB/<strong>MySQL</strong></div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-database"></span></span> MongoDB</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <strong>Nginx</strong></div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-database"></span></span> Redis</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> Software-Defined Networking</div>
-          </div>
+        <div class="panel-block">
+          <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-github"></span></span>
+          GitHub: github.com/ct-martin
         </div>
+        <!--sse-->
+          <div class="panel-block">
+            <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-envelope"></span></span>
+            Email: hello@ctmartin.me
+          </div>
+          <div class="panel-block">
+            <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-linkedin-square"></span></span>
+            <span>LinkedIn: linkedin.com/in/ct-martin</span>
+          </div>
+          <div class="panel-block">
+            <span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-handshake-o"></span></span>
+            Handshake: rit.joinhandshake.com/users/6938294
+          </div>
+        <!--/sse-->
       </div>
-      <div class="column">
-        <div class="card" style="margin-top: .75rem;">
-          <div class="card-content">
-            <h4 class="subtitle">Platforms</h4>
-          </div>
-          <div class="panel">
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> Cloudflare</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <strong>Docker</strong></div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> Heroku</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-linux"></span></span> <span><strong>Linux</strong>; CentOS, Debian, Fedora, Ubuntu</span></div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <span><strong>Virtualization</strong> (QEMU/KVM-based, ESXi)</span></div>
-          </div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="card" style="margin-top: .75rem;">
-          <div class="card-content">
-            <h4 class="subtitle">Design</h4>
-          </div>
-          <div class="panel">
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-file-image-o"></span></span> Adobe Illustrator</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-file-image-o"></span></span> Adobe Photoshop</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-file-image-o"></span></span> Adobe XD</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> MVC</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-cloud"></span></span> <span><strong>Responsive</strong> &amp; <strong>Mobile-First</strong> Design</span></div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-gamepad"></span></span> Unity</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-gamepad"></span></span> Unreal Engine</div>
-            <div class="panel-block"><span class="panel-icon"><span aria-hidden="true" class="fa fa-fw fa-wordpress"></span></span> WordPress</div>
-          </div>
-        </div>
-      </div>
+
+      <p>Full Stack Developer, Creative Technologist, Computing Generalist; Studying New Media Interactive Development at Rochester Institute of Technology.</p>
+
+
     </div>
   </article>
 </template>

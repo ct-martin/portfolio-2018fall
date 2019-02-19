1206d37003465d3265ee57ccfaa3c05a20cbd872
ct-martin <ct-martin@users.noreply.github.com>
2019-02-18 21:19:41 -0500

    Use HTTPS for Google document viewer

--- components/PDFViewer.vue
+++ components/PDFViewer.vue
@@ -12,9 +12,9 @@ export default {
     },
   computed: {
     srcFull() {
-      // Google Drive Viewer: `http://docs.google.com/gview?url=${this.src}&embedded=true`
+      // Google Drive Viewer: `https://docs.google.com/gview?url=${this.src}&embedded=true`
       // PDF.js: `https://mozilla.github.io/pdf.js/web/viewer.html?file=${this.src}`
-      return `http://docs.google.com/gview?url=${this.src}&embedded=true`;
+      return `https://docs.google.com/gview?url=${this.src}&embedded=true`;
     }
   }
 }

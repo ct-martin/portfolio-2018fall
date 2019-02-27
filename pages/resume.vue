803d247358f4e24c10d98e0df94582a578045254
ct-martin <ct-martin@users.noreply.github.com>
2019-02-27 04:49:06 -0500

    Add print instructions to resume & direct download link

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,6 +1,10 @@
 <template>
   <article class="container">
-  <PDFViewer src="https://new.ctmartin.me/resume.pdf" />
+    <p class="content">For print options click the arrow icon on the top-right of the viewer</p>
+    <PDFViewer src="https://new.ctmartin.me/resume.pdf" />
+
+    <br />
+    <a class="button is-link is-fullwidth" href="/resume.pdf" target="_blank"><span class="fa fa-fw fa-file-pdf-o"></span> Download</a>
   </article>
 </template>
 

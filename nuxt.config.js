bf0c9ad148f34750cd97b60c80f6035f7c16864a
ct-martin <ct-martin@users.noreply.github.com>
2019-02-27 03:21:01 -0500

    Add a little SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,11 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { 'og:site_name': pkg.description },
+      { 'twitter:title': pkg.description },
+      { 'application-name': pkg.description },
+      { 'twitter:site': '@_ctmartin' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },

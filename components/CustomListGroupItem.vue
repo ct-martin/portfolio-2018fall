d448a74a40ca97cd0ca65cde9139a70a611dc1ef
ct-martin <ct-martin@users.noreply.github.com>
2019-02-27 03:21:39 -0500

    Have component open external links in a new tab

--- components/CustomListGroupItem.vue
+++ components/CustomListGroupItem.vue
@@ -6,7 +6,7 @@
     </div>
     <!--<span class="fa fa-chevron-right"></span>-->
   </nuxt-link>
-  <a v-else :href="project.url" class="card is-block">
+  <a v-else :href="project.url" class="card is-block" target="_blank">
     <div class="card-content">
       <p class="subtitle"><span v-if="project.icon" :class="project.icon" class="fa fa-fw"></span>{{ project.name }}</p>
       <p>{{ project.description }}</p>

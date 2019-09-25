7fbccc4a9ed7959e2078a2f80cdf1bf4dd1a4e26
Christian Martin <ct-martin@users.noreply.github.com>
2019-09-24 23:15:33 -0400

    Support Buefy's new Navbar component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,69 +1,16 @@
 <template>
-  <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-    <div class="container">
-      <div class="navbar-brand">
-        <nuxt-link class="navbar-item has-text-weight-bold" to="/">Christian Martin</nuxt-link>
-
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarMain">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarMain" class="navbar-menu">
-        <div class="navbar-start">
-          <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
-          <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link>
-          <nuxt-link class="navbar-item" to="/talks">Talks</nuxt-link>
-          <nuxt-link class="navbar-item" to="/articles">Articles</nuxt-link>
-        </div>
-        <div class="navbar-end">
-          <nuxt-link class="navbar-item" to="/about">About</nuxt-link>
-        </div>
-      </div>
-    </div>
-  </nav>
+  <b-navbar fixed-top wrapper-class="container">
+    <template slot="brand">
+      <b-navbar-item class="has-text-weight-bold" tag="nuxt-link" to="/">Christian Martin</b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item tag="nuxt-link" to="/">Home</b-navbar-item>
+      <b-navbar-item tag="nuxt-link" to="/projects">Projects</b-navbar-item>
+      <b-navbar-item tag="nuxt-link" to="/talks">Talks</b-navbar-item>
+      <b-navbar-item tag="nuxt-link" to="/articles">Articles</b-navbar-item>
+    </template>
+    <template slot="end">
+      <b-navbar-item tag="nuxt-link" to="/about">About</b-navbar-item>
+    </template>
+  </b-navbar>
 </template>
-
-<script>
-// export b/c component
-export default {
-  mounted: function() {
-    // https://bulma.io/documentation/components/navbar/#navbar-menu
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
-
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-
-        });
-      });
-    }
-
-    // added bit to close navbar upon item click
-    document.querySelectorAll('.navbar').forEach(nav => {
-      nav.querySelectorAll('a:not(.navbar-burger)').forEach(el => {
-        el.addEventListener('click', () => {
-          // This bit is copied from the Bulma code above
-          // Remove the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          nav.querySelector('.navbar-burger').classList.remove('is-active');
-          nav.querySelector('.navbar-menu').classList.remove('is-active');
-        });
-      });
-    });
-  }
-}
-</script>

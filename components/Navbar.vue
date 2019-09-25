c8956e605a143491cae1f79371de36d9a528056d
Christian Martin <ct-martin@users.noreply.github.com>
2019-09-25 00:17:21 -0400

    Link blog

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,12 +5,13 @@
     </template>
     <template slot="start">
       <b-navbar-item tag="nuxt-link" to="/">Home</b-navbar-item>
+      <b-navbar-item tag="nuxt-link" to="/about">About</b-navbar-item>
       <b-navbar-item tag="nuxt-link" to="/projects">Projects</b-navbar-item>
       <b-navbar-item tag="nuxt-link" to="/talks">Talks</b-navbar-item>
       <b-navbar-item tag="nuxt-link" to="/articles">Articles</b-navbar-item>
     </template>
     <template slot="end">
-      <b-navbar-item tag="nuxt-link" to="/about">About</b-navbar-item>
+      <b-navbar-item tag="a" href="https://blog.ctmartin.me/">Blog</b-navbar-item>
     </template>
   </b-navbar>
 </template>

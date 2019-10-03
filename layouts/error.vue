1ea26a25841f6ed319142398e6d54d602f39da94
Christian Martin <ct-martin@users.noreply.github.com>
2019-10-03 15:27:53 -0400

    Custom 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+    <article class="container">
+        <h1 class="title" v-if="error.statusCode === 404">404 - Page Not Found</h1>
+        <h1 class="title" v-else>An error occurred</h1>
+
+        <p>When you look into the <code>void</code>, the <code>void</code> also looks into you.</p>
+        <nuxt-link to="/">Back to home</nuxt-link>
+    </article>
+</template>
+
+<script>
+export default {
+    props: ['error']
+}
+</script>

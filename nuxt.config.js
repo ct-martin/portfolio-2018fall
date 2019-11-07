09f8f443452538464cc5d80cd880adeae9de3bfe
Christian Martin <ct-martin@users.noreply.github.com>
2019-11-07 11:47:05 -0500

    SEO lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,9 @@ module.exports = {
   */
   head: {
     title: "Portfolio | Christian Martin",
-    lang: 'en',
+    htmlAttrs: {
+      lang: 'en',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

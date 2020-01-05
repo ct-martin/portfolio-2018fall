90d4477908127466f883d3dfef3001bda87ad91c
Christian Martin <ct-martin@users.noreply.github.com>
2020-01-04 16:38:53 -0800

    Update copyright year

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
     </main>
     <footer class="footer has-background-dark">
       <div class="container">
-        <p>&copy; 2019 Christian Martin</p>
+        <p>&copy; 2020 Christian Martin</p>
       </div>
     </footer>
   </div>

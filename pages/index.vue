f16d8c32bf377a4d100cbb2bb439873765326026
Christian Martin <ct-martin@users.noreply.github.com>
2020-01-08 23:42:12 -0800

    WIP: Revised homepage

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
       </div>
     </section>
 
-    <br >
+    <!--<br >
     <div id="heroCards" class="container">
       <h3 class="title">Projects</h3>
       <div class="columns">
@@ -35,7 +35,69 @@
     <div id="homeCards" class="container">
       <h3 class="title">Articles &amp; Talks</h3>
       <custom-list-group source="content" tag="home-more" />
-    </div>
+    </div>-->
+
+    <!-- <nuxt-link v-for="project in this.$store.state.talks.list" :to="project.url" :key="project.name" class="card is-block"> -->
+    <br>
+    <section class="container">
+      <b-dropdown
+        aria-role="list"
+        hoverable
+        multiple
+        v-model="selectedCategories"
+      >
+        <button class="button" slot="trigger">
+          <span class="fa fa-fw fa-filter"></span>
+          <span>Categories ({{selectedCategories.length}})</span>
+          <span class="fa fa-fw fa-caret-down"></span>
+        </button>
+
+        <b-dropdown-item
+          aria-role="listitem"
+          v-for="cat in meta.categories"
+          :key="cat"
+          :value="cat"
+        >
+          <span>{{cat}}</span>
+        </b-dropdown-item>
+      </b-dropdown>
+
+      <b-dropdown
+        aria-role="list"
+        hoverable
+        multiple
+        v-model="selectedTags"
+      >
+        <button class="button" slot="trigger">
+          <span class="fa fa-fw fa-tag"></span>
+          <span>Tags ({{selectedTags.length}})</span>
+          <span class="fa fa-fw fa-caret-down"></span>
+        </button>
+
+        <b-dropdown-item
+          aria-role="listitem"
+          v-for="tag in meta.tags"
+          :key="tag"
+          :value="tag"
+        >
+          <span>{{tag}}</span>
+        </b-dropdown-item>
+      </b-dropdown>
+      <hr>
+      <div v-if="filteredItems.length">
+        <div class="columns is-multiline">
+          <nuxt-link v-for="project in filteredItems" :to="project.url" :key="project.name" class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen">
+            <div class="card">
+              <div class="card-content">
+                <h4 class="title is-4"><span v-if="project.icon" :class="project.icon" class="fa fa-fw"/> {{ project.name }}</h4>
+                <p>{{ project.description }}</p>
+              </div>
+            </div>
+          </nuxt-link>
+        </div>
+      </div>
+      <p v-else>Nothing to see; try a less specific filter</p>
+    </section>
   </div>
 </template>
 
@@ -45,6 +107,39 @@ import CustomListGroup from '~/components/CustomListGroup.vue'
 export default {
   components: {
     CustomListGroup
+  },
+  computed: {
+    filteredItems() {
+      return this.$store.state['content'].list
+        .filter(item => 
+          this.selectedCategories.includes(item.category)
+          && this.selectedTags.some(
+            (t) => item.tags.includes(t)
+          )
+        );
+    }
+  },
+  data() {
+    let categories = [];
+    let tags = [];
+    this.$store.state['content'].list.forEach((i) => {
+      if(!categories.includes(i.category)) {
+        categories.push(i.category);
+      }
+      i.tags.forEach((t) => {
+        if(!t.startsWith('home-') && !tags.includes(t)) {
+          tags.push(t);
+        }
+      })
+    });
+    return {
+      meta: {
+        categories,
+        tags
+      },
+      selectedCategories: Array.from(categories),
+      selectedTags: Array.from(tags)
+    }
   }
 }
 </script>

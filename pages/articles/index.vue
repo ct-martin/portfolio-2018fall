029d04a95208f055b75dfdf0260f7482005ae54f
Christian Martin <ct-martin@users.noreply.github.com>
2020-01-22 16:14:09 -0500

    Minor visual improvement

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -2,6 +2,7 @@
   <article class="container">
     <h1 class="title"><span class="fa fa-book fa-fw"/> Articles</h1>
     <p>Here is a subset of the posts on my blog, excluding those for classes or projects (which would be on the Projects page).</p>
+    <br>
     <a class="button is-link is-fullwidth" href="https://blog.ctmartin.me/" style="margin-bottom:1.5em;">Click here for entire blog</a>
     <a v-for="project in this.$store.state.articles.list" :href="project.url" :key="project.name" class="card is-block">
       <div class="card-content">
